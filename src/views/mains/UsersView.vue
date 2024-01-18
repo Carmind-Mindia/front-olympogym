040c4cbc47f6d21eb9b43f44e04af7f2d6688ff2
Alexis <alexisfonzos_@hotmail.com>
2024-01-17 23:35:57 -0300

    Add icons and avatar to UsersView.vue

--- src/views/mains/UsersView.vue
+++ src/views/mains/UsersView.vue
@@ -1,5 +1,7 @@
 <script lang="ts">
+import IconArrowRight from '@/components/icons/IconArrowRight.vue';
 import IconLupa from '@/components/icons/IconLupa.vue';
+import IconPersonAvatar from '@/components/icons/IconPersonAvatar.vue';
 import IconPlus from '@/components/icons/IconPlus.vue';
 import ToggleButton from '@/components/ToggleButton.vue';
 import type { User } from '@/services/user-hub.api.types';
@@ -27,26 +29,35 @@ export default defineComponent({
     computed: {
         ...mapStores(authStore)
     },
-    components: { IconLupa, IconPlus, ToggleButton }
+    components: { IconLupa, IconPlus, ToggleButton, IconPersonAvatar, IconArrowRight }
 })
 </script>
 
 <template>
   <div class="flex flex-col items-center w-full h-full">
-    <div class="flex h-[120px] bg-bg_menu w-full flex-col px-4">
+    <!-- Top bar -->
+    <div class="flex h-[120px] min-h-[120px] bg-bg_menu w-full flex-col px-4">
       <div class="flex flex-row justify-between w-full my-4">
         <IconLupa class="w-[45px] h-[45px] cursor-pointer" />
         <IconPlus class="w-[45px] h-[45px] cursor-pointer" />
       </div>
       <div class="flex flex-row w-full gap-2">
-        <ToggleButton class="h-[30px]" label="Actividad reciente" :isSelect="true" />
-        <ToggleButton class="h-[30px]" label="Actividad antigua" :isSelect="true" />
+        <ToggleButton class="max-h-[30px]" label="Actividad reciente" :isSelect="true" />
+        <ToggleButton class="max-h-[30px]" label="Actividad antigua" :isSelect="true" />
       </div>
     </div>
-    <div class="w-full text-black">
-      <div v-for="(user, index) in users" :key="index" class="flex justify-between p-2 border-b border-gray-200">
-        <div class="text-gray-700">{{ user.FirstName }}</div>
-        <div class="text-gray-700">{{ user.LastName }}</div>
+
+    <!-- Content -->
+    <div class="flex flex-col w-full h-full p-3 text-black ">
+      <div v-for="(user, index) in users" :key="index" class="flex flex-row items-center h-16 p-2 border-b rounded-lg bg-primary">.
+        <IconPersonAvatar class="w-[48px] h-[48px]" />
+        <div class="flex flex-col items-start flex-1 h-full mx-3">
+          <div class="flex flex-row">
+            <div class="text-gray-700">{{ user.FirstName }}</div>
+            <div class="text-gray-700">{{ user.LastName }}</div>
+          </div>
+        </div>
+        <IconArrowRight class="w-[32px] h-[32px]" />
       </div>
     </div>
   </div>
